2eefe2a0a7afe3820d30359cc5287bc4c30862b6
honghanhh <hanh.usth@gmail.com>
2019-03-19 20:56:01 +0700

    Update labwork 3

--- 03.practical.work.server.setup/03.practical.work.server.setup.c
+++ 03.practical.work.server.setup/03.practical.work.server.setup.c
@@ -11,6 +11,7 @@ int main() {
     if ((sockfd=socket(AF_INET, SOCK_STREAM, 0)) < 0) 
     {
         printf("Error creating socket\n");
+        return -1;
     }
 
     memset(&saddr, 0, sizeof(saddr));
@@ -21,16 +22,23 @@ int main() {
     if (bind(sockfd, (struct sockaddr *) &saddr, sizeof(saddr)) < 0) 
     {
         printf("Error binding\n");
+        return -1;
     }
 
     if (listen(sockfd, 5) < 0) 
     {
         printf("Error listening\n");
+        return -1;
     }
 
     clen=sizeof(caddr);
     if ((clientfd=accept(sockfd, (struct sockaddr *) &caddr, &clen)) < 0) 
     {
         printf("Error accepting connection\n");
+        return -1;
     }
+
+    printf("Client (maybe human or not) connects!");
+    return 0;
+
 }
