9f2dceeae5ee640d6ce48772c2e739ff7359c987
honghanhh <hanh.usth@gmail.com>
2019-03-21 00:09:22 +0700

    Ladwork 5 - data transfer, take turn

---
+++ 05.practical.work.turn/05.practical.work.client.turn.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    struct sockaddr_in saddr;
+    struct hostent *h;
+    int sockfd;
+    unsigned short port = 8784;
+    char *host_name = malloc(sizeof(char) * 128);
+    char *ip_addr;
+    char mess[200];
+
+    //get server hostname from program arguments || STDIN if no arguments
+    if (argc == 2)
+    {
+        host_name = argv[1];
+        printf("%s \n", host_name);
+    }
+    else
+    {
+        printf(" Please enter a host name:");
+        scanf("%s", host_name);
+    }
+
+    //resolve IP address & STDOUT
+    h = gethostbyname(host_name);
+    if (!h)
+        perror(" Host unknown \n");
+    ip_addr = inet_ntoa(*(struct in_addr *)h->h_name);
+    printf(" Hostname is: %s , IP address is: %s \n ", h->h_name, ip_addr);
+
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        perror("Error creating socket\n");
+        close(sockfd);
+        return -1;
+    }
+    memset(&saddr, 0, sizeof(saddr));
+    saddr.sin_family = AF_INET;
+    memcpy((char *)&saddr.sin_addr.s_addr, h->h_addr_list[0], h->h_length);
+
+    struct in_addr **bo = (struct in_addr **)h->h_addr_list;
+    struct in_addr hanh = *bo[0];
+    printf("%s\n", inet_ntoa(hanh));
+
+    saddr.sin_port = htons(port);
+    if (connect(sockfd, (struct sockaddr *)&saddr, sizeof(saddr)) < 0)
+    {
+        perror("Cannot connect\n");
+        close(sockfd);
+        return -1;
+    }
+    else
+    {
+        printf("Sucessfully connected!\n");
+    }
+
+    while (1)
+    {
+        printf("Client: ");
+        scanf("%[^\n]%*c", mess);
+        write(sockfd, mess, sizeof(mess));
+
+        if (read(sockfd, mess, sizeof(mess)) > 0)
+        {
+            printf("Server: %s\n", mess);
+        }
+    }
+}
---
+++ 05.practical.work.turn/05.practical.work.server.turn.c
@@ -0,0 +1,57 @@
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+int main()
+{
+    int sockfd, clen, clientfd;
+    struct sockaddr_in saddr, caddr;
+    unsigned short port = 8784;
+    char mess[200];
+
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("Error creating socket\n");
+        return -1;
+    }
+
+    memset(&saddr, 0, sizeof(saddr));
+    saddr.sin_family = AF_INET;
+    saddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    saddr.sin_port = htons(port);
+
+    if (bind(sockfd, (struct sockaddr *)&saddr, sizeof(saddr)) < 0)
+    {
+        printf("Error binding\n");
+        return -1;
+    }
+
+    if (listen(sockfd, 5) < 0)
+    {
+        printf("Error listening\n");
+        return -1;
+    }
+    
+    while (1)
+    {
+        clen = sizeof(caddr);
+        if ((clientfd = accept(sockfd, (struct sockaddr *)&caddr, &clen)) < 0)
+        {
+            printf("Error accepting connection\n");
+        }
+
+        while (1)
+        {
+            if (read(clientfd, mess, sizeof(mess)) > 0)
+            {
+                printf("Client: %s\n", mess);
+            }
+
+            printf("Server: ");
+            scanf("%[^\n]%*c", mess);
+            write(clientfd, mess, sizeof(mess));
+        }
+    }
+}
